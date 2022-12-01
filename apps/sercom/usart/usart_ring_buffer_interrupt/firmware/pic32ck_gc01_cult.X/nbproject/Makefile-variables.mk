1d8eed54b27300fb8735b685b01057902896a49c
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-12-01 19:12:54 +0530

    [SERCOM] Create SERCOM USART Ring-buffer demo for PIC32CK_GC01 (Unicorn)
    MH3-72404
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

---
+++ apps/sercom/usart/usart_ring_buffer_interrupt/firmware/pic32ck_gc01_cult.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32ck_gc01_cult configuration
+CND_ARTIFACT_DIR_pic32ck_gc01_cult=dist/pic32ck_gc01_cult/production
+CND_ARTIFACT_NAME_pic32ck_gc01_cult=pic32ck_gc01_cult.X.production.hex
+CND_ARTIFACT_PATH_pic32ck_gc01_cult=dist/pic32ck_gc01_cult/production/pic32ck_gc01_cult.X.production.hex
