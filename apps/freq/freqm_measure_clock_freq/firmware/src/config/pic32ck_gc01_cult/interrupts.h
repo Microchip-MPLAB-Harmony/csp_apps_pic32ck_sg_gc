fe80914f2d1e2b8131c1efdb36cf3d348364f306
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-12-16 15:30:35 +0530

    [FREQ] Regenerated demo for freq_freqm_measure_clock_freq_pic32ck_gc01_cult
    MH3-73517
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

--- apps/freq/freqm_measure_clock_freq/firmware/src/config/pic32ck_gc01_cult/interrupts.h
+++ apps/freq/freqm_measure_clock_freq/firmware/src/config/pic32ck_gc01_cult/interrupts.h
@@ -61,6 +61,7 @@ void Reset_Handler (void);
 void NonMaskableInt_Handler (void);
 void HardFault_Handler (void);
 void FREQM_InterruptHandler (void);
+void SERCOM5_USART_InterruptHandler (void);
 
 
 
