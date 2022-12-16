80de7acbded4091e8f5c3e6eddb575a2bf22cb67
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-12-16 15:31:29 +0530

    [PM] Regenerated demo for pm_pm_wakeup_eic_pic32ck_gc01_cult
    MH3-73517
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

--- apps/pm/pm_wakeup_eic/firmware/src/config/pic32ck_gc01_cult/interrupts.h
+++ apps/pm/pm_wakeup_eic/firmware/src/config/pic32ck_gc01_cult/interrupts.h
@@ -62,6 +62,7 @@ void NonMaskableInt_Handler (void);
 void HardFault_Handler (void);
 void SysTick_Handler (void);
 void EIC_EXTINT_10_InterruptHandler (void);
+void SERCOM5_USART_InterruptHandler (void);
 
 
 
