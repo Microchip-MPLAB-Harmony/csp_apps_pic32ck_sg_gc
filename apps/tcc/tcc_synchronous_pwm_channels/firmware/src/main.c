47a3870774c24eaeab698dc3b757a9d042e071f1
Sumanth Naga Venkata Ranga Suraneni <Sumanth.Suraneni@microchip.com>
2023-02-23 11:43:05 +0530

    [TCC] Add TCC Synchronous PWM channels demo for PIC32CK_GC01
    MH3-74501

---
+++ apps/tcc/tcc_synchronous_pwm_channels/firmware/src/main.c
@@ -0,0 +1,86 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+/* Duty cycle increment value */
+#define DUTY_INCREMENT (10U)
+
+/* Save PWM period */
+uint32_t period;
+
+/* This function is called after TCC period event */
+void TCC_PeriodEventHandler(uint32_t status, uintptr_t context)
+{
+    /* duty cycle values */
+    static uint32_t duty0 = 0U;
+        
+    TCC1_PWM32bitDutySet(TCC1_CHANNEL0, duty0);
+     
+    /* Increment duty cycle values */
+    duty0 += DUTY_INCREMENT;
+    
+    if (duty0 > period)
+        duty0 = 0U;
+    
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    /* Register callback function for period event */
+    TCC1_PWMCallbackRegister(TCC_PeriodEventHandler, (uintptr_t)NULL);
+    
+    /* Read the period */
+    period = TCC1_PWM32bitPeriodGet();
+    
+    /* Start PWM*/
+    TCC1_PWMStart();
+
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
---
+++ apps/tcc/tcc_synchronous_pwm_channels/readme.md
@@ -0,0 +1,62 @@
+---
+parent: Harmony 3 peripheral library application examples for PIC32CK SG/GC Family
+title: TCC PWM generation
+has_children: false
+has_toc: false
+---
+
+[![MCHP](https://www.microchip.com/ResourcePackages/Microchip/assets/dist/images/logo.png)](https://www.microchip.com)
+
+# TCC PWM generation
+
+This example application shows how to use the TCC to generate a 2 or 3 phase PWM signals for motor control with dead time.
+
+## Description
+
+TCC module is configured to generate synchronous 2 or 3 phase PWM signals with dead time. The duty cycle of the PWM signals is updated in the period interrupt handler.
+
+## Downloading and building the application
+
+To clone or download this application from Github, go to the [main page of this repository](https://github.com/Microchip-MPLAB-Harmony/csp_apps_pic32ck_gc) and then click **Clone** button to clone this repository or download as zip file.
+This content can also be downloaded using content manager by following these [instructions](https://github.com/Microchip-MPLAB-Harmony/contentmanager/wiki).
+
+Path of the application within the repository is **apps/tcc/tcc_synchronous_pwm_channels/firmware** .
+
+To build the application, refer to the following table and open the project using its IDE.
+
+| Project Name      | Description                                    |
+| ----------------- | ---------------------------------------------- |
+| pic32ck_gc01_cult.X    | MPLABX Project for [PIC32CK GC01 Curiosity Ultra board]()|
+|||
+
+## Setting up the hardware
+
+The following table shows the target hardware for the application projects.
+
+| Project Name| Board|
+|:---------|:---------:|
+| pic32ck_gc01_cult.X    | [PIC32CK GC01 Curiosity Ultra board]()|
+|||
+
+### Setting up [PIC32CK GC01 Curiosity Ultra board]()
+
+- Connect the Debug USB port on the board to the computer using a micro USB cable
+
+## Running the Application
+
+1. Build and Program the application using its IDE
+2. Observe the high-side and low-side PWM waveforms on oscilloscope
+3. Observe the dead time between the high side (**1us**) and the low side (**0.5us**)
+4. Observe the pwm frequency to be **25 KHz**
+
+[PIC32CK GC01 Curiosity Ultra board]() pin details:
+
+|Signal Name| Pad   | Pin |
+|-----------|-------|-----|
+| TCC1_WO0-PWMH  | PC15  | Pin 16 of EXT2 connector  |
+| TCC1_WO4-PWML  | PC19  | Pin 9 of EXT1 connector |
+
+
+[PIC32CK GC01 Curiosity Ultra board]() waveforms:
+
+  ![output](images/output_tcc_synchronous_pwm_channels.png)
