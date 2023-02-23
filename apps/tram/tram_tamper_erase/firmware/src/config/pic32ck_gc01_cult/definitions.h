06b7972719842c68655e22d74b24a0f67fcbaf65
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-02-23 21:17:32 +0530

    [TRAM] Regenerated demo for tram_tram_tamper_erase_pic32ck_gc01_cult
    MH3-74688
    
    Signed-off-by: Jignesh Moradiya <Jignesh.Moradiya@microchip.com>

--- apps/tram/tram_tamper_erase/firmware/src/config/pic32ck_gc01_cult/definitions.h
+++ apps/tram/tram_tamper_erase/firmware/src/config/pic32ck_gc01_cult/definitions.h
@@ -55,8 +55,8 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/usart/plib_sercom5_usart.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/rtc/plib_rtc.h"
 
 // DOM-IGNORE-BEGIN
