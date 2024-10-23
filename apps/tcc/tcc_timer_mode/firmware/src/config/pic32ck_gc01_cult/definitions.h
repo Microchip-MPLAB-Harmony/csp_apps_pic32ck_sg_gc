cdbfde24c42f99d46922724ab8cb4f4fe7b749c3
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-23 12:38:10 +0530

    [TCC] Regenerated demo for tcc__tcc_timer_mode__firmware__pic32ck_gc01_cult.X
    MH3-99642

--- apps/tcc/tcc_timer_mode/firmware/src/config/pic32ck_gc01_cult/definitions.h
+++ apps/tcc/tcc_timer_mode/firmware/src/config/pic32ck_gc01_cult/definitions.h
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/tcc/plib_tcc0.h"
 #include "peripheral/evsys/plib_evsys.h"
+#include "peripheral/tcc/plib_tcc0.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
@@ -65,13 +65,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CK2051GC01144"
-#define DEVICE_ARCH			 "CORTEX-M33"
-#define DEVICE_FAMILY		 "PIC32CK"
-#define DEVICE_SERIES		 "PIC32CKGC01"
+#define DEVICE_NAME          "PIC32CK2051GC01144"
+#define DEVICE_ARCH          "CORTEX-M33"
+#define DEVICE_FAMILY        "PIC32CK"
+#define DEVICE_SERIES        "PIC32CKGC01"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 120000000
+#define CPU_CLOCK_FREQUENCY 120000000U
 
 // *****************************************************************************
 // *****************************************************************************
