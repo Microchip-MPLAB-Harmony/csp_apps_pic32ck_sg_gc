a7f0d0b4099ce4099f479871958b9a643a1b36b8
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 18:08:45 +0000

    [APP] Regenerated demo for dma__dma_crc32_generate__firmware
    MH3C-262

--- apps/dma/dma_crc32_generate/firmware/src/config/pic32ck_gc01_cult/definitions.h
+++ apps/dma/dma_crc32_generate/firmware/src/config/pic32ck_gc01_cult/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -55,8 +55,8 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/dma/plib_dma0.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "peripheral/sercom/usart/plib_sercom5_usart.h"
 #include "peripheral/pm/plib_pm.h"
+#include "peripheral/sercom/usart/plib_sercom5_usart.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
