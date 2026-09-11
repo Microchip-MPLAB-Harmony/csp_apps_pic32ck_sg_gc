6b064d8e24a27b03955829a454377477f44eb2ab
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2026-09-11 17:41:50 +0530

    [TCC] Regenerated demo for apps__tcc__tcc_capture_mode__firmware__pic32ck_gc01_cult.X
    MH3C-1102

--- apps/tcc/tcc_capture_mode/firmware/src/config/pic32ck_gc01_cult/initialization.c
+++ apps/tcc/tcc_capture_mode/firmware/src/config/pic32ck_gc01_cult/initialization.c
@@ -96,8 +96,6 @@
 #pragma config FUSES_USERCFG1_FUCFG2_BRCFGUDSSMOR_BOR_TRIP = 0x3U
 #pragma config FUSES_USERCFG1_FUCFG3_HYST_BOR_VDDIO = SET
 #pragma config FUSES_USERCFG1_FUCFG3_BOR_TRIP_VDDIO = 0x3U
-#pragma config FUSES_USERCFG1_FUCFG3_HYST_BOR_VDDIOB = SET
-#pragma config FUSES_USERCFG1_FUCFG3_BOR_TRIP_VDDIOB = 0x3U
 #pragma config FUSES_USERCFG1_FUCFG3_HYST_BOR_VDDA = SET
 #pragma config FUSES_USERCFG1_FUCFG3_BOR_TRIP_VDDA = 0x3U
 #pragma config FUSES_USERCFG1_FUCFG3_HYST_BOR_VDDREG = SET
@@ -227,8 +225,6 @@
 #pragma config FUSES_USERCFG2_FUCFG2_BRCFGUDSSMOR_BOR_TRIP = 0x3U
 #pragma config FUSES_USERCFG2_FUCFG3_HYST_BOR_VDDIO = SET
 #pragma config FUSES_USERCFG2_FUCFG3_BOR_TRIP_VDDIO = 0x3U
-#pragma config FUSES_USERCFG2_FUCFG3_HYST_BOR_VDDIOB = SET
-#pragma config FUSES_USERCFG2_FUCFG3_BOR_TRIP_VDDIOB = 0x3U
 #pragma config FUSES_USERCFG2_FUCFG3_HYST_BOR_VDDA = SET
 #pragma config FUSES_USERCFG2_FUCFG3_BOR_TRIP_VDDA = 0x3U
 #pragma config FUSES_USERCFG2_FUCFG3_HYST_BOR_VDDREG = SET
@@ -324,10 +320,10 @@
 // *****************************************************************************
 // *****************************************************************************
 /* Following MISRA-C rules are deviated in the below code block */
-/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
-/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
-/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
-/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+/* MISRA C-2023 Rule 7.2 - Deviation record ID - H3_MISRAC_2023_R_7_2_DR_1 */
+/* MISRA C-2023 Rule 11.1 - Deviation record ID - H3_MISRAC_2023_R_11_1_DR_1 */
+/* MISRA C-2023 Rule 11.3 - Deviation record ID - H3_MISRAC_2023_R_11_3_DR_1 */
+/* MISRA C-2023 Rule 11.8 - Deviation record ID - H3_MISRAC_2023_R_11_8_DR_1 */
 
 
 
@@ -369,19 +365,19 @@
  ********************************************************************************/
 static void STDIO_BufferModeSet(void)
 {
-    /* MISRAC 2012 deviation block start */
-    /* MISRA C-2012 Rule 21.6 deviated 2 times in this file.  Deviation record ID -  H3_MISRAC_2012_R_21_6_DR_3 */
+    /* MISRAC 2023 deviation block start */
+    /* MISRA C-2023 Rule 21.6 deviated 2 times in this file.  Deviation record ID -  H3_MISRAC_2023_R_21_6_DR_3 */
 
     /* Make stdin unbuffered */
     setbuf(stdin, NULL);
 
     /* Make stdout unbuffered */
     setbuf(stdout, NULL);
-    /* MISRAC 2012 deviation block end */
+    /* MISRAC 2023 deviation block end */
 }
 
 
-/* MISRAC 2012 deviation block end */
+/* MISRAC 2023 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -396,8 +392,8 @@ static void STDIO_BufferModeSet(void)
 void SYS_Initialize ( void* data )
 {
 
-    /* MISRAC 2012 deviation block start */
-    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+    /* MISRAC 2023 deviation block start */
+    /* MISRA C-2023 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2023_R_2_2_DR_1 */
 
     STDIO_BufferModeSet();
 
@@ -417,9 +413,9 @@ void SYS_Initialize ( void* data )
     TCC0_CompareInitialize();
 
 	SYSTICK_TimerInitialize();
-    SERCOM5_USART_Initialize();
-
     PM_Initialize();
+
+    SERCOM5_USART_Initialize();
 
     EIC_Initialize();
 
@@ -427,7 +423,7 @@ void SYS_Initialize ( void* data )
     NVIC_Initialize();
 
 
-    /* MISRAC 2012 deviation block end */
+    /* MISRAC 2023 deviation block end */
 }
 
 /*******************************************************************************
