d63a4a993113f32d86d7290182b60fefc25daf17
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2026-09-11 17:41:29 +0530

    [DMA] Regenerated demo for apps__dma__dma_crc32_generate__firmware__pic32ck_gc01_cult.X
    MH3C-1102

--- apps/dma/dma_crc32_generate/firmware/src/config/pic32ck_gc01_cult/definitions.h
+++ apps/dma/dma_crc32_generate/firmware/src/config/pic32ck_gc01_cult/definitions.h
@@ -55,8 +55,8 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/dma/plib_dma0.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/usart/plib_sercom5_usart.h"
+#include "peripheral/pm/plib_pm.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
